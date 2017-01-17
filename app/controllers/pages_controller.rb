9d1dcde1e43a6bf5a7170bcd3f56994fade657d8
Alex Hower <alex.c.hower@gmail.com>
2017-01-17 14:20:32 +0000

    debugging css errors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,8 @@ class PagesController < ApplicationController
     #raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
     require 'twilio-ruby'
 
-    account_sid = ENV['TWILIO_ACCOUNT_SID'] #account_sid = 'ACee44210f6d2954b784af10e5ddbdc164' # Your Account SID from www.twilio.com/user/account
-    auth_token = ENV['TWILIO_AUTH_TOKEN'] #auth_token = '3748de84cdb04fa784ac7163eff31e23'   # Your Auth Token from www.twilio.com/user/account
+    account_sid = ENV['TWILIO_ACCOUNT_SID'] # Your Account SID from www.twilio.com/user/account
+    auth_token = ENV['TWILIO_AUTH_TOKEN']   # Your Auth Token from www.twilio.com/user/account
 
     @client = Twilio::REST::Client.new(account_sid, auth_token)
     message = @client.account.messages.create(
